dff74d2d0b23d7a8203a00f234271624b7c7b703
dev28237582 <>
2021-02-18 13:16:10 +1100

    Lint factories on start, clean up author spec

--- spec/factories/common_factory.rb
+++ spec/factories/common_factory.rb
@@ -1,19 +1,22 @@
 FactoryBot.define do
+
   factory :user, class: User do
     username { Faker::Internet.username }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
   end
 
-  factory :public_author, class: Author do
-    name { Faker::Name.name }
-    public { true }
-    user { create(:user) }
-  end
+  factory :author, class: Author do
 
-  factory :private_author, class: Author do
-    name { Faker::Name.name }
-    public { false }
-    user { create(:user) }
+    association :user, factory: :user
+
+    factory :disavowed_author do
+      name { Faker::Internet.username }
+      public { false }
+    end
+
+    factory :avowed_author do
+      public { true }
+    end
   end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,26 +2,46 @@ require 'rails_helper'
 
 RSpec.describe Author do
 
+  context 'by default' do
+
+    subject(:author) { create(:author) }
+
+    it 'is publicly avowed' do
+      expect(subject).to be_public
+    end
+  end
+
   context 'when publicly avowed' do
 
-    subject(:author) { create(:public_author) }
+    subject(:author) { create(:avowed_author) }
+    let(:controlling_user_name) { author.user.username }
 
     context 'when formatting names' do
 
-      subject( :author_name ) { author.formatted_name }
+      subject(:author_name) { author.formatted_name }
 
+      it 'is formatted with a prefix of u/ to signify that it is avowed by a user' do
+        expect(subject).to start_with('u/')
+      end
+
+      it 'contains the controlling user name' do
+        expect(subject).to include(controlling_user_name)
+      end
     end
-
   end
 
   context 'when not publicly avowed' do
 
-    subject(:author) { create(:private_author) }
-    let( :controlling_user_name ) { author.user.username }
+    subject(:author) { create(:disavowed_author) }
+    let(:controlling_user_name) { author.user.username }
+
+    it 'should not be publicly avowed' do
+      expect(subject).to_not be_public
+    end
 
     context 'when formatting names' do
 
-      subject( :author_name ) { author.formatted_name }
+      subject(:author_name) { author.formatted_name }
 
       it 'is formatted with a prefix of a/ to signify that it stands alone' do
         expect(subject).to start_with('a/')
@@ -35,7 +55,5 @@ RSpec.describe Author do
         expect(subject).to_not include(controlling_user_name)
       end
     end
-
   end
-
 end
